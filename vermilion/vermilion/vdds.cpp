26876890daf47b8310b00662eb09323a8a346753
Peter M. Petrakis <peter.petrakis@protonmail.com>
2019-08-25 20:42:27 -0400

    gotos are bad

--- vermilion/vermilion/vdds.cpp
+++ vermilion/vermilion/vdds.cpp
@@ -584,7 +584,8 @@ void vglLoadDDS(const char* filename, vglImageData* image)
 
     if (file_header.magic != DDS_MAGIC)
     {
-        goto done_close_file;
+        fclose(f);
+        return;
     }
 
     if (file_header.std_header.ddspf.dwFourCC == DDS_FOURCC_DX10)
@@ -593,12 +594,18 @@ void vglLoadDDS(const char* filename, vglImageData* image)
     }
 
     if (!vgl_DDSHeaderToImageDataHeader(file_header, image))
-        goto done_close_file;
+    {
+        fclose(f);
+        return;
+    }
 
     image->target = vgl_GetTargetFromDDSHeader(file_header);
 
     if (image->target == GL_NONE)
-        goto done_close_file;
+    {
+        fclose(f);
+        return;
+    }
 
     size_t current_pos = ftell(f);
     size_t file_size;
@@ -639,7 +646,6 @@ void vglLoadDDS(const char* filename, vglImageData* image)
         depth >>= 1;
     }
 
-done_close_file:
     fclose(f);
 }
 
